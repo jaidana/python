94e5346681ea045e36200159d1d78d1331ca8b65
jaidana <37110348+jaidana@users.noreply.github.com>
2019-06-08 16:00:44 +0200

    Add files via upload

---
+++ liste2.py
@@ -0,0 +1,42 @@
+homme = ['Jean', 'Rafa', 'Gina', 'Jeremi', 'Mohammed', 'Ping', 'Geroge']
+femme = ['Dana', 'Mariyana', 'Pauline', 'Fatima', 'kathija', 'Nadine']
+#teste vide
+def vide(homme):
+    if (homme) == []:
+        return True
+    else:
+        return False
+#test permiere
+def prem(femme):
+    if (femme) >= []:
+        return femme[0]
+    else:
+        return False
+#test reste
+def reste(femme):
+    if (femme) >= []:
+        return femme[1:]
+    else:
+        return False
+#question 1
+def postion(femme):
+    if (femme) >= ['4']:
+        return femme[2:3]
+    else:
+        return False
+#une liste      
+class MyContainer:
+    def __contains__(self, value):
+        return value is not None   #not none is evry thing 
+
+# cardinal
+def card(femme):
+    if vide(femme):
+        return 0
+    else:
+        return (1+card(reste(femme)))
+#
+class mySizeable:
+    def __len__(self):
+        return 18
+    
