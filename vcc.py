876a8f2b4422f64ffddbcab8678a62bac96fd9d9
jaidana <37110348+jaidana@users.noreply.github.com>
2019-07-05 12:58:45 +0200

    Add files via upload

---
+++ vcc.py
@@ -0,0 +1,18 @@
+##Ingerdient = namedtuple('ingredient', 'prix qte')
+##
+##class Sandwich:
+##    def __init__(self,sauce):
+##        self.sauce = sauce
+##        self._ing = dict()
+##
+##    @property
+##    def prix(self)
+
+    
+class ClassA:
+    pass
+class ClassB:
+    pass
+class ClassC:
+    pass
+
